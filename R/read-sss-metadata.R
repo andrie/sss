1f28e7578e9347943663bcd40f0767fb62f646a0
Andrie de Vries <apdevries@gmail.com>
2017-03-28 09:46:45 +0100

    Use argument `x` instead of longer names (e.g. SSSfilename) #7; Use assert_that() to validate input #5

--- R/read-sss-metadata.R
+++ R/read-sss-metadata.R
@@ -5,28 +5,43 @@
 #'
 #' This function reads a .sss XML metadata file.  The .sss standard defines a standard survey structure
 #'
-#' @param SSSfilename Name of .sss file containing the survey metadata
+#' @param x Name of .sss file containing the survey metadata
+#' @param SSSfilename No longer used. Use `x` instead.
 #' @export 
 #' @seealso [parseSSSmetadata()], [read.sss()], [readSSSdata()]
 #' @keywords read
 #' @importFrom xml2 read_xml
 #' @example inst/examples/example-read-sss-metadata.R
-readSSSmetadata <- function(SSSfilename){
-  read_xml(SSSfilename)
+readSSSmetadata <- function(x, SSSfilename){
+  if(!missing(SSSfilename)) {
+    warning("The argument SSSfilename has been deprecated. Use `x` instead.")
+    if(missing(x)) x <- SSSfilename
+  }
+  assert_that(is.character(x))
+  assert_that(file.exists(x))
+  read_xml(x)
 }
 
+
+is.xml_document <- function(x)inherits(x, "xml_document")
 
 #' Parses a triple-s XML (sss) metadata file, as specified by the triple-s XML standard.  
 #'
 #' This function reads and parses a .sss XML metadata file as well as its associated .asc data file. The .sss standard defines a standard survey structure
 #' 
-#' @param XMLdoc An XML document - as returned by [XML::xml()], or [readSSSmetadata()]
+#' @param x An XML document - as returned by [XML::xml()], or [readSSSmetadata()]
+#' @param XMLdoc No longer used. Use `x` instead.
 #' @keywords parse
 #' @export 
 #' @importFrom xml2 xml_attrs xml_children xml_child xml_attr xml_length xml_contents
 #' @seealso readSSSmetadata, read.sss, readSSSdata
-parseSSSmetadata <- function(XMLdoc){
-  r <- xml_child(XMLdoc, "survey/record")
+parseSSSmetadata <- function(x, XMLdoc){
+  if(!missing(XMLdoc)) {
+    warning("The argument XMLdox has been deprecated. Use `x` instead")
+    if(missing(x)) x <- XMLdoc
+  }
+  assert_that(is.xml_document(x))
+  r <- xml_child(x, "survey/record")
   format <- if("format" %in% names(xml_attrs(r))) xml_attrs(r)[["format"]] else "fixed"
   skip   <- if("skip"   %in% names(xml_attrs(r))) xml_attrs(r)[["skip"]] else 0
   variables <- fastdf(
@@ -42,15 +57,22 @@ parseSSSmetadata <- function(XMLdoc){
 
 #' Reads a triple-s XML (asc) data file, as specified by the triple-s XML standard.
 #'
-#' This function reads and parses a .sss XML metadata file as well as its associated .asc data file. The .sss standard defines a standard survey structure
+#' This function reads an `.asc`` data file.
 #'
-#' @param ascFilename Name of .asc file containing the survey metadata
+#' @param x Name of .asc file containing the survey metadata
+#' @param ascFilename No longer used. Use `x` instead.
 #' @export 
 #' @seealso [read.sss()], [readSSSmetadata()]
 #' @keywords parse
 #' @example inst/examples/example-read-sss-metadata.R
-readSSSdata <- function(ascFilename){
-  suppressWarnings(scan(ascFilename, sep="\n", what="character"))
+readSSSdata <- function(x, ascFilename){
+  if(!missing(ascFilename)) {
+    warning("The argument ascFilename has been deprecated. Use `x` instead")
+    if(missing(x)) x <- ascFilename
+  }
+  assert_that(is.character(x))
+  assert_that(file.exists(x))
+  suppressWarnings(scan(x, sep="\n", what="character"))
 }
 
 
